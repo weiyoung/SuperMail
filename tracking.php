f78f4655ce1e41f1b283f39c20c1a3491cb2f512
z4m2b <z4m2b@ugrad.cs.ubc.ca>
2020-04-04 02:29:03 -0700

    added functionality for tracking

--- tracking.php
+++ tracking.php
@@ -1,40 +1,41 @@
 <?php
+
+function showThisOrder($obConn,$sql) {
+  $rsResult = mysqli_query($obConn, $sql) or die(mysqli_error($obConn));
+  if(mysqli_num_rows($rsResult)>0) {
+    echo "<table width=\"100%\" border=\"0\" cellspacing=\"2\"cellpadding=\"0\"><tr align=\"center\" bgcolor=\"#CCCCCC\">";
+    $i = 0;
+
+    //retrive field names
+    while ($i < mysqli_num_fields($rsResult)) {
+      $field = mysqli_fetch_field_direct($rsResult, $i);
+      $fieldName=$field->name;
+      echo "<td><strong>$fieldName</strong></td>";
+      $i = $i + 1;
+    }
+
+    //field names retrieved. now dump info
+    $bolWhite=true;
+    while ($row = mysqli_fetch_assoc($rsResult)) {
+      echo $bolWhite ? "<tr bgcolor=\"#CCCCCC\">" : "<tr bgcolor=\"#FFF\">";
+        $bolWhite = !$bolWhite; 
+        foreach ($row as $data) {
+          echo "<td>$data</td>"; 
+        }
+        echo "</tr>";
+    }
+
+  }
+
+}
+
 include 'connect.php';
 $conn = OpenCon();
-$sql = "SELECT order_id, customer_id, eid, initial_date, delivery_date, priority, delivery_status, pricing, receiver_id FROM deliveryorder";
-$result = $conn->query($sql);
-if ($result->num_rows > 0) {
-    echo "List of Delivery Orders";
-
-    echo "<table>
-    <tr>
-    <th class='border-class'>order_id</th>
-    <th class='border-class'>customer_id</th>
-    <th class='border-class'>eid</th>
-    <th class='border-class'>initial_date</th>
-    <th class='border-class'>delivery_date</th>
-    <th class='border-class'>priority</th>
-    <th class='border-class'>delivery_status</th>
-    <th class='border-class'>pricing</th>
-    <th class='border-class'>receiver_id</th>
-    </tr>";
-    // output data of each row
-    while($row = $result->fetch_assoc()) { 
-        echo"
-        <tr><td class='border-class'>".$row["order_id"]."</td>
-        <td class='border-class'>".$row["customer_id"]."</td>
-        <td class='border-class'>".$row["eid"]."</td>
-        <td class='border-class'>".$row["initial_date"]."</td>
-        <td class='border-class'>".$row["delivery_date"]."</td>
-        <td class='border-class'>".$row["priority"]."</td>
-        <td class='border-class'>".$row["delivery_status"]."</td>
-        <td class='border-class'>".$row["pricing"]."</td>
-        <td class='border-class'>".$row["receiver_id"]."</td>
-        </tr>";
-    }
-    echo "</table>";
-} else {
-  echo "no results :(";
-}
+$where_cols = $_POST['where_cols'];
+$sql = "SELECT order_id, customer_id, eid, initial_date, delivery_date, priority, delivery_status, pricing, receiver_id 
+        FROM deliveryorder
+        WHERE order_id like '%$where_cols%'";
+showThisOrder($conn, $sql);
 CloseCon($conn);
+
 ?>
